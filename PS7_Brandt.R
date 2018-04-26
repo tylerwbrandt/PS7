0a6cd2db86fc3d4b79fa6d0f45d83c7bec530f72
Tyler Brandt <tylerbrandt02@gmail.com>
2018-04-26 00:59:45 -0500

    Found neighborhoods with most occurrences
    This was really just the same process as number 2 but grouping by neighborhoood instead of by crime type.

--- PS7_Brandt.R
+++ PS7_Brandt.R
@@ -8,12 +8,20 @@ View(crime_data)
 
 # 2. Crimes per day by type of crime
 crime_data <- mutate(crime_data, cleaned_description = gsub("-.*$", "", crime_data$Description))
-daily_rates_march <- crime_data %>%
+daily_rates_crime_type <- crime_data %>%
   group_by(cleaned_description) %>%
   summarise(count = n()/31) %>%
   arrange(desc(count))
-daily_rates_march
+daily_rates_crime_type
 
 ## Larceny has the most occurrences
 
-# 3. 
+# 3. Number of crime per day by neighborhood
+daily_rates_neighborhood <- crime_data %>%
+  group_by(Neighborhood) %>%
+  summarise(count = n()/31) %>%
+  arrange(desc(count))
+daily_rates_neighborhood
+
+## Neighborhood 35 has the most crimes
+
